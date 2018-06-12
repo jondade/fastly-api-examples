aa0f6a4657d8fabdbce0579b5f8405e90b3fafde
Jonathan Dade <jonathandade@gmail.com>
2018-06-12 16:05:19 +0100

    Moved lib directory to root of repo. Couple of TODO notes and some more token stuff.

---
+++ lib/common_functions.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+#
+# This file contains some code common to all the example scripts. This is kept
+# here to keep the example files as clean / clear as possible.
+
+# Common variables
+API_URL="https://api.fastly.com"
+CREDENTIALS_FILE="$HOME/.fastly/api_credentials.sh"
+CURL="$(which curl) -H Accept:application/json"
+
+function startup() {
+  parse_args $@
+  check_credentials
+  source $CREDENTIALS_FILE
+  TOKEN_HEADER="-H Fastly-key:$TOKEN"
+}
+
+function parse_args(){
+  while getopts "dh" opt; do
+    case $opt in
+      d )
+        echo "Debug mode: ON"
+        set_debug
+        ;;
+      h)
+        #TODO: write some useful help information
+        echo "help stuffs"
+        exit 0
+        ;;
+      default )
+        ;;
+    esac
+  done
+}
+
+function set_debug() {
+  set -e
+  set -x
+  CURL=$CURL" -sv"
+}
+
+function check_credentials() {
+  if [[ ! -f $CREDENTIALS_FILE ]]; then
+    echo "No token found at $CREDENTIALS_FILE."
+    echo "Please run generate_api_credentials.sh to create them."
+    exit 1
+  fi
+}
+
+function echo_split() {
+  echo
+  echo
+  echo -n "============================================"
+  echo "============================================"
+  echo
+  echo -n -e ">>>> "
+}
